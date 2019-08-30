23ca466e6726b142ac2e39fe8c98f77a9658dfc2
acandael <info@anthonycandaele.com>
2019-08-30 10:49:08 +0200

    create api router

--- server.js
+++ server.js
@@ -4,13 +4,18 @@ const cors = require('cors');
 const errorhandler = require('errorhandler');
 const express = require('express');
 
+const apiRouter = require('./api/api');
+
 const app = express();
 const PORT = process.env.PORT || 4000;
 
 app.use(bodyParser.json());
 app.use(cors());
-app.use(errorhandler());
 app.use(morgan('dev'));
+
+app.use('/api', apiRouter);
+
+app.use(errorhandler());
 
 app.listen(PORT, () => {
   console.log(`Listening on port: ${PORT}`);
