ec33cfb0120aef88fab2e2630249c381fbef0fbc
acandael <info@anthonycandaele.com>
2019-09-01 16:36:10 +0200

    create issue table and create issue routes

--- api/series.js
+++ api/series.js
@@ -6,6 +6,8 @@ const db = new sqlite3.Database(
   process.env.TEST_DATABASE || './database.sqlite'
 );
 
+const issuesRouter = require('./issues.js');
+
 seriesRouter.param('seriesId', (req, res, next, seriesId) => {
   const sql = 'SELECT * FROM Series WHERE Series.id = $seriesId';
   const values = {
@@ -22,6 +24,8 @@ seriesRouter.param('seriesId', (req, res, next, seriesId) => {
     }
   });
 });
+
+seriesRouter.use('/:seriesId/issues', issuesRouter);
 
 seriesRouter.get('/', (req, res, next) => {
   db.all('SELECT * FROM Series', (err, series) => {
@@ -94,6 +98,28 @@ seriesRouter.put('/:seriesId', (req, res, next) => {
           res.status(200).json({ series: series });
         }
       );
+    }
+  });
+});
+
+seriesRouter.delete('/:seriesId', (req, res, next) => {
+  const issueSql = 'SELECT * FROM Issue WHERE Issue.series_id = $seriesId';
+  const issueValues = { $seriesId: req.params.seriesId };
+  db.get(issueSql, issueValues, (error, issue) => {
+    if (error) {
+      next(error);
+    } else if (issue) {
+      res.sendStatus(400);
+    } else {
+      const deleteSql = 'DELETE FROM Series WHERE Series.id = $seriesId';
+      const deleteValues = { $seriesId: req.params.seriesId };
+      db.run(deleteSql, deleteValues, error => {
+        if (error) {
+          next(error);
+        } else {
+          res.sendStatus(204);
+        }
+      });
     }
   });
 });
