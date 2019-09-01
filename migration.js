10cd94ce3c8dba0a71eb9c008bb7b7faa5fd0051
acandael <info@anthonycandaele.com>
2019-09-01 11:12:19 +0200

    create Series table

--- migration.js
+++ migration.js
@@ -9,4 +9,9 @@ db.serialize(function() {
     biography TEXT NOT NULL,
     is_currently_employed INTEGER NOT NULL DEFAULT 1
   )`);
+  db.run(`CREATE TABLE IF NOT EXISTS Series (
+    id INTEGER PRIMARY KEY,
+    name TEXT NOT NULL,
+    description TEXT NOT NULL
+  )`);
 });
